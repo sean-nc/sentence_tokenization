da5aad1176e5a8587b7d6d20d16cb8006b265a87
Sean <sean.n.cordes@gmail.com>
2018-12-18 12:47:46 -0500

    updated with id

--- rewrite_excel.py
+++ rewrite_excel.py
@@ -7,10 +7,23 @@ excel.columns = ['paragraphs']
 
 paragraph_list = [val for idx,val in excel['paragraphs'].items()]
 
-new_paragraph_list = []
+new_paragraph_list =[]
+new_paragraph_dict = {
+  'id': [],
+  'description': []
+}
 
 for paragraph in paragraph_list:
   new_paragraph_list.extend(paragraph_to_sentences(paragraph))
 
-df = pd.DataFrame(new_paragraph_list)
+count = 1
+
+for paragraph in paragraph_list:
+  if paragraph == "----------":
+    count += 1
+    continue
+  new_paragraph_dict['description'].extend([paragraph])
+  new_paragraph_dict['id'].extend([count])
+
+df = pd.DataFrame(new_paragraph_dict)
 df.to_excel('equity_research_jd_in_sentences.xlsx', header=False, index=False)
